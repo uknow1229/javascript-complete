213e0035dbeb6a71f500f73ce3d8553c9fb4066b
saya <uknow.love1229@gmail.com>
2023-07-13 12:41:31 +0900

    finish: 4-53

--- BASIC/main4.js
+++ BASIC/main4.js
@@ -13,4 +13,17 @@ ok = 1 === 1;
 ok = 'hello' === 'hello';
 ok = 1 !== 2;
 ok = 1 == '1';
+ok = 1 === '1';
+ok = 1 != '2';
+
+const coffee1 = { name: 'Cafe Latte' };
+const coffee2 = { name: 'Cafe Latte' };
+const coffee3 = coffee1;
+ok = coffee1 === coffee3;
+ok = coffee1.name === coffee2.name;
+
+const fruits1 = ['apple', 'banana'];
+const fruits2 = ['apple', 'banana'];
+ok = fruits1 === fruits2;
+
 console.log(ok);
