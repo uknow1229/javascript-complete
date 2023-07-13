31a21ca5e37997d9c57646cfc8e8c092bbb52a10
saya <uknow.love1229@gmail.com>
2023-07-13 12:28:03 +0900

    finish: 4-52

--- BASIC/main4.js
+++ BASIC/main4.js
@@ -2,9 +2,15 @@ let ok = false;
 let maybeOK = false;
 
 if (ok) {
-  console.log('OK!');
+  // console.log('OK!');
 } else if (maybeOK) {
   console.log('maybe OK...');
 } else {
   console.log('NO!');
 }
+
+ok = 1 === 1;
+ok = 'hello' === 'hello';
+ok = 1 !== 2;
+ok = 1 == '1';
+console.log(ok);
