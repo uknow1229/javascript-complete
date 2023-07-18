d78cf5c34e43cf96a2a8aa3fbbdec0a81fe317d5
saya <uknow.love1229@gmail.com>
2023-07-18 17:03:04 +0900

    finish: 10-160

--- BASIC/main10.js
+++ BASIC/main10.js
@@ -12,4 +12,26 @@ delete fruits[10];
 fruits[hello] = 'hello';
 fruits.length = 100;
 fruits.length = 2;
-console.log(fruits);
+
+fruits = ['apple', , 'banana', , , 'grape'];
+for (const fruit of fruits) {
+  // console.log(fruit);
+}
+fruits = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+fruits = new Array('apple', 'banana', 'grape');
+fruits = new Array(1, 2, 3);
+fruits = new Array(1);
+fruits = new Array('hello');
+fruits = Array('apple', 'banana', 'grape');
+fruits = Array(1, 2, 3);
+fruits = Array(1);
+fruits = Array('hello');
+fruits = Array.of('apple', 'banana', 'grape');
+fruits = Array.of(1, 2, 3);
+fruits = Array.of(1);
+fruits = Array.of('hello');
+console.log(fruits[1], [1]);
