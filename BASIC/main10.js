d76d1f2e25221862973f999dcb17621feae6b953
saya <uknow.love1229@gmail.com>
2023-07-18 16:47:05 +0900

    finish: 10-159

--- BASIC/main10.js
+++ BASIC/main10.js
@@ -1,8 +1,15 @@
-let fruits = ['apple', 'banana'];
 let arrayLikeObject = {
   0: 'apple',
   1: 'banana',
   length: 2,
 };
 arrayLikeObject.__proto__ = Array.prototype;
+
+let fruits = ['apple', 'banana'];
+fruits[2] = 'grape';
+fruits[10] = 'orange';
+delete fruits[10];
+fruits[hello] = 'hello';
+fruits.length = 100;
+fruits.length = 2;
 console.log(fruits);
